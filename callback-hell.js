117f038c02829ed5db7621c251d09b015546acb5
Dario Castromonte <dario.castromonte@gmail.com>
2021-02-22 21:30:33 -0300

    Ejercicio Resuelto

--- callback-hell.js
+++ callback-hell.js
@@ -23,7 +23,7 @@ const sueldos = [
         sueldo: 2000
     }
 ];
-
+//Esta funcion me da el nombre por el id 
 const getEmpleadoById = (id, callback) =>{
     
     const empleado = empleados.find( (e) => e.id === id );
@@ -36,13 +36,37 @@ const getEmpleadoById = (id, callback) =>{
     
 
 };
-
+//Esta funcion me da el sueldo por el id
 const getSueldoById = (id,callback) =>{
 
-}
+    const sueldo = sueldos.find( (e) => e.id === id);
+    
+    if(sueldo){
+        callback(null,sueldo);
+        return;
+    }
+    callback(new Error('El empleado no recibe sueldo'));
 
-const id = 5 ;
+};
 
+
+
+const id = 3 ;
+
+//Asi llamaria a la funcion si solo quiero saber el sueldo 
+/*
+getSueldoById(id,(error, sueldo) => {
+    if (error){
+        console.log('Error!!!!...');
+        console.log(error);
+        return;
+    }
+
+    console.log(`El empleado recibe: ${sueldo.sueldo}`);
+})
+*/
+//Asi llamaria a la funcion si solo quiero saber el nombre del empleado
+/*
 getEmpleadoById(id,(error, empleado) => {
 
     if(error){
@@ -54,4 +78,32 @@ getEmpleadoById(id,(error, empleado) => {
     //aca poner el lo que recibo de la funcion del sueldo y sumarlo al console log
     console.log(`El empleado: ${empleado.nombre}`);
 });
+*/
+
+getEmpleadoById(id,(error, empleado) => {
+
+    if(error){
+        console.log('Error!!!!....');
+        console.log(error);
+        return;
+    }
+    
+    getSueldoById(id,(error, sueldo) => {
+        if (error){
+            console.log('Error!!!!...');
+            console.log(error);
+            return;
+        }
+
+        console.log(`El empleado: ${empleado.nombre} tiene un sueldo de: ${sueldo.sueldo}`);
+
+        
+    })
+
+    
+});
+
+
+
+
 
